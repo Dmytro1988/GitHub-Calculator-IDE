6593ca19d33d087166ea9c540e1dc974599429f9
Dmytro1988 <yaholnyk88@gmail.com>
2023-04-02 12:15:33 +0300

    Update app info

--- src/App/Main.java
+++ src/App/Main.java
@@ -2,6 +2,6 @@ package App;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        System.out.println("My git app. Super!");
     }
 }
